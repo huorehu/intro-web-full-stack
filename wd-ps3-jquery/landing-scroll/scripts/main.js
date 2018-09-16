d384020d7014e89b3fb54038166448e27789fc23
huorehu <huorehu@gmail.com>
2018-09-16 18:49:35 +0300

    add a smooth navigation for links in the header

--- wd-ps3-jquery/landing-scroll/scripts/main.js
+++ wd-ps3-jquery/landing-scroll/scripts/main.js
@@ -1,8 +1,16 @@
+const SCROLL_TIME = 1500;
+const linksId = ["#product", "#about-us", "#contact-us", "#register"];
+const NAVIGATION_LINKS = $(".navigation").children();
+
 function scrollPage(clckElem, scrollTo) {
-  clckElem.click(function () {
+  clckElem.onclick = function () {
     const animatedElem = $('html, body');
-    animatedElem.animate({scrollTop: 0}, 1000);
-  });
+    animatedElem.animate({scrollTop: scrollTo.offset().top}, SCROLL_TIME);
+  };
 }
 
-scrollPage($('#scroll-up-btn'), 0);
+scrollPage($('#scroll-up-btn')[0], $("header"));
+
+for (let i = 0; i < NAVIGATION_LINKS.length; i++) {
+  scrollPage(NAVIGATION_LINKS[i], $(linksId[i]));
+}
