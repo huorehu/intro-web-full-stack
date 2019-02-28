9070e8dad303d4db90679ca5b2da7998aae31211
huorehu <huorehu@gmail.com>
2019-02-28 15:35:34 +0200

    format code and change data which stored in current_user variable

--- webdev-st1-atm/atm-main.js
+++ webdev-st1-atm/atm-main.js
@@ -32,99 +32,129 @@ const ATM = {
     // authorization
     auth: function (number, pin) {
         if (this.is_auth) {
-            this.printMessage(this.users[this.current_user].number, this.ACTION_MESSAGE['already_auth'], "green");
+            this.printMessage(
+                this.current_user.number,
+                this.ACTION_MESSAGE['already_auth'],
+                "green");
             return;
         }
 
         if (!/\d{4}/.test(number) || !/\d{3}/.test(pin)) {
-            this.printMessage("not authorized", this.ACTION_MESSAGE['unsucc_auth'], "red");
+            this.printMessage(
+                "not authorized",
+                this.ACTION_MESSAGE['unsucc_auth'],
+                "red");
             return;
         }
 
         for (let i = 0; i < this.users.length; i++) {
             if (this.users[i].number === number && this.users[i].pin === pin) {
                 this.is_auth = true;
-                this.current_user = i;
+                this.current_user = this.users[i];
                 this.current_type = this.users[i].type;
-                this.printMessage(this.users[this.current_user].number, this.ACTION_MESSAGE['succ_auth'], "green");
+                this.printMessage(
+                    this.current_user.number,
+                    this.ACTION_MESSAGE['succ_auth'],
+                    "green");
                 return;
             }
         }
 
-        this.printMessage("not authorized", this.ACTION_MESSAGE['unsucc_auth'], "red");
+        this.printMessage(
+            "not authorized",
+            this.ACTION_MESSAGE['unsucc_auth'],
+            "red");
     },
 
     // check current debit
     check: function () {
         if (!this.is_auth) {
-            this.printMessage("not authorized", "attempt " + this.ACTION_MESSAGE['check'] + ", " + this.ACTION_MESSAGE['permission_denied'], "red");
+            this.printMessage(
+                "not authorized",
+                "attempt " + this.ACTION_MESSAGE['check'] + ", " + this.ACTION_MESSAGE['permission_denied'],
+                "red");
             return;
         }
 
-        this.printMessage(this.users[this.current_user].number, "debit: " + this.users[this.current_user].debit, "green");
+        this.printMessage(
+            this.current_user.number,
+            "debit: " + this.current_user.debit,
+            "green");
     },
 
     // get cash - available for user only
     getCash: function (amount) {
-        const ACTION = this.ACTION_MESSAGE['withdrawal'] + " " + amount;
+        const ACTION = this.ACTION_MESSAGE['withdrawal'] + " " + this.getAmountMessage(amount);
 
-        if (!this.validateAccess(ACTION, (a, b) =>  a === b, amount)) {
+        if (!this.validateAccess(ACTION, (a, b) => a === b, amount)) {
             return;
         }
 
         if (!this.isEnoughDebit(amount)) {
-            this.printMessage(this.users[this.current_user].number, this.ACTION_MESSAGE['insufficient_debit'], "orange");
+            this.printMessage(
+                this.current_user.number,
+                this.ACTION_MESSAGE['insufficient_debit'],
+                "orange");
             return;
         }
 
         if (!this.isEnoughCash(amount)) {
-            this.printMessage(this.users[this.current_user].number, this.ACTION_MESSAGE['not_enough_cash'], "orange")
+            this.printMessage(
+                this.current_user.number,
+                this.ACTION_MESSAGE['not_enough_cash'],
+                "orange");
             return;
         }
 
-        this.users[this.current_user].debit -= amount;
+        this.current_user.debit -= amount;
         this.cash -= amount;
-        this.printMessage(this.users[this.current_user].number, ACTION, "green");
+        this.printMessage(
+            this.current_user.number,
+            ACTION,
+            "green");
     },
 
     // load cash - available for user only
     loadCash: function (amount) {
-        const ACTION = this.ACTION_MESSAGE['top_up_account'] + " " + amount;
+        const ACTION = this.ACTION_MESSAGE['top_up_account'] + " " + this.getAmountMessage(amount);
 
-        if (!this.validateAccess(ACTION, (a, b) =>  a === b, amount)) {
+        if (!this.validateAccess(ACTION, (a, b) => a === b, amount)) {
             return;
         }
 
-        this.users[this.current_user].debit += amount;
+        this.current_user.debit += amount;
         this.cash += amount;
-        this.printMessage(this.users[this.current_user].number, this.ACTION_MESSAGE['top_up_account']
-            + ", current debit: " + this.users[this.current_user].debit, "green");
+        this.printMessage(
+            this.current_user.number,
+            this.ACTION_MESSAGE['top_up_account'] + ", current debit: " + this.current_user.debit,
+            "green");
     },
 
     // load cash to ATM - available for admin only - EXTENDED
     load_cash: function (addition) {
         const ACTION = this.ACTION_MESSAGE['load_cash'] + " " + addition;
 
-        if (!this.validateAccess(ACTION, (a, b) =>  a !== b, addition)) {
+        if (!this.validateAccess(ACTION, (a, b) => a !== b, addition)) {
             return;
         }
 
         this.cash += addition;
-        this.printMessage(this.users[this.current_user].number, ACTION + ", current ATM cash: " + this.cash, "green");
+        this.printMessage(
+            this.current_user.number,
+            ACTION + ", current ATM cash: " + this.cash,
+            "green");
     },
 
     // get report about cash actions - available for admin only - EXTENDED
     getReport: function () {
         if (this.current_type === "admin") {
             for (let action of this.cash_actions) {
-                let message_color;
+                let message_color = 'green';
 
                 if (action.includes("not authorized") || action.includes("denied")) {
                     message_color = 'red';
                 } else if (action.includes("insufficient") || action.includes("attempt")) {
                     message_color = 'orange';
-                } else {
-                    message_color = 'green';
                 }
 
                 console.log("%c" + action, this.MESSAGE_COLOR[message_color]);
@@ -132,14 +162,19 @@ const ATM = {
             return;
         }
 
-        this.printMessage(this.is_auth ? this.users[this.current_user].number : "not authorized",
-            "attempt read ATM report, " + this.ACTION_MESSAGE['permission_denied'], "red");
+        this.printMessage(
+            this.is_auth ? this.current_user.number : "not authorized",
+            "attempt read ATM report, " + this.ACTION_MESSAGE['permission_denied'],
+            "red");
     },
 
     // log out
     logout: function () {
         if (this.is_auth) {
-            this.printMessage(this.users[this.current_user].number, this.ACTION_MESSAGE['logout'], "green");
+            this.printMessage(
+                this.current_user.number,
+                this.ACTION_MESSAGE['logout'],
+                "green");
             this.init();
         } else {
             console.log("%cYou are not authorized!", this.MESSAGE_COLOR['orange']);
@@ -159,7 +194,8 @@ const ATM = {
         this.cash_actions = ["Init ATM: cash " + this.cash + " - " + this.getCurrentDate()];
     },
 
-    printMessage: function(user, message, color) {
+    printMessage: function (
+        user, message, color) {
         this.log_action(user, message);
         console.log("%c" + message, this.MESSAGE_COLOR[color]);
     },
@@ -175,11 +211,11 @@ const ATM = {
     },
 
     isEnoughDebit: function (amount) {
-        return this.users[this.current_user].debit - amount > 0;
+        return this.current_user.debit - amount > 0;
     },
 
     isEnoughCash: function (amount) {
-        return this.cash - amount >= 0
+        return this.cash - amount >= 0;
     },
 
     isCorrectAmount: function (amount) {
@@ -188,21 +224,34 @@ const ATM = {
 
     validateAccess: function (action, compareFunc, amount) {
         if (!this.is_auth) {
-            this.printMessage("not authorized", "attempt " + action + ", " + this.ACTION_MESSAGE['permission_denied'], "red");
+            this.printMessage(
+                "not authorized",
+                "attempt " + action + ", " + this.ACTION_MESSAGE['permission_denied'],
+                "red");
             return false;
         }
 
         if (compareFunc(this.current_type, "admin")) {
-            this.printMessage(this.users[this.current_user].number, "attempt " + action + ", " + this.ACTION_MESSAGE['permission_denied'], "red")
+            this.printMessage(
+                this.current_user.number,
+                "attempt " + action + ", " + this.ACTION_MESSAGE['permission_denied'],
+                "red");
             return false;
         }
 
         if (!this.isCorrectAmount(amount)) {
-            this.printMessage(this.users[this.current_user].number, "attempt " + action, "orange");
+            this.printMessage(
+                this.current_user.number,
+                "attempt " + action,
+                "orange");
             return false;
         }
 
         return true;
+    },
+
+    getAmountMessage(intStr) {
+        return intStr ? intStr + "" : "\u003Cinvalid input\u003E"
     }
 };
 ATM.init();
