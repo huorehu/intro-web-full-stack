cd176c2bf550dbd7a50463e621eb3b93cb7da588
huorehu <huorehu@gmail.com>
2019-04-12 16:37:33 +0300

    add chat login page

---
+++ wd-ps5-ajax/public/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Easy Chat</title>
+</head>
+<body>
+  <div class="wrapper">
+    <form id="register" action="handler.php" method="post">
+      <h1>Easy Chat</h1>
+      <label for="name">Enter your name</label>
+      <input class="field" type="text" placeholder="John Doe">
+      <label for="password">Enter your password</label>
+      <input class="field" type="password" placeholder="">
+      <input type="submit" class="button">
+    </form>
+  </div>
+</body>
+</html>
